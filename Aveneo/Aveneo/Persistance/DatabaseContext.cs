a8a5bfa7af90a3298fc9b4138963b3aeafc90c7e
Mateusz Korolow <mattioo@windowslive.com>
2017-11-06 15:41:18 +0100

    Add DbContext and InitialMigration

---
+++ Aveneo/Aveneo/Persistance/DatabaseContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aveneo.Persistance
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+    }
+}
--- Aveneo/Aveneo/Startup.cs
+++ Aveneo/Aveneo/Startup.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Aveneo.Persistance;
 
 namespace Aveneo
 {
@@ -22,6 +20,7 @@ namespace Aveneo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Aveneo")));
             services.AddMvc();
         }
 
