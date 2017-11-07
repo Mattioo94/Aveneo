cb0a0d69595d94eccfcded7235069497a0222e7a
Mateusz Korolow <mattioo@windowslive.com>
2017-11-07 12:07:55 +0100

    Add rendering response in main (Home) component

--- Aveneo/Aveneo/ClientApp/components/Home.tsx
+++ Aveneo/Aveneo/ClientApp/components/Home.tsx
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { NavMenu } from "ClientApp/components/NavMenu";
 
-export class Home extends React.Component<RouteComponentProps<{}>, {}> {
+interface MyComponentState { Answer: string }
+
+export class Home extends React.Component<RouteComponentProps<{}>, MyComponentState> {
+
+    constructor() {
+        super();
+        this.state = { Answer: '' };
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
     public render() {
         return <div>
             <div className="form-group">
                 <label htmlFor="number">NIP/KRS/REGON</label>
                 <input id="number" className="form-control" />
                 <button onClick={this.onSubmit} className="btn btn-primary">Submit</button>
+            </div>
+                {this.state.Answer}
+            <div id="answer" className="form-group">
             </div>
         </div>;
     }
@@ -20,7 +33,11 @@ export class Home extends React.Component<RouteComponentProps<{}>, {}> {
         xhr.open('get', `api/Companies/${inputValue}`, true);
         xhr.onload = () => {
             var response = JSON.parse(xhr.responseText);
-            alert(response.name);
+
+            this.setState({
+                Answer: response.name
+            });
+
         }
 
         xhr.send();
